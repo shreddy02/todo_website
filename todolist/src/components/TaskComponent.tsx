5047f7030df37549468d5d6758e58c19529b2244
shreddy02 <shreddy1729@gmail.com>
2023-02-01 21:33:39 +0530

    readme data added

--- todolist/src/components/TaskComponent.tsx
+++ todolist/src/components/TaskComponent.tsx
@@ -194,3 +194,11 @@ export default TaskComponent;
 {/* <Checkbox {...label} icon={<DeleteOutlineIcon />} checkedIcon={<DeleteIcon />} sx = {{position: 'absolute', right: '120px'}} color = "error"/>
               <Checkbox {...label} icon={<EditIcon />} sx = {{marginRight: '2px', position: 'absolute', right: '150px'}}color="primary" /> */}
 
+
+              // todo-list-website
+              // A todo-list website using typescript in the front-end and back-end
+              
+              // Todo Website using the concept of Eisenhower matrix, where tasks are divided into four categories: Important and Urgent Important and Not Urgent Not Important and Urgent Not Important and Not Urgent
+              
+              // A user can add a new task, update a task, delete a task, also mark tasks as important, urgent, and completed.
+
